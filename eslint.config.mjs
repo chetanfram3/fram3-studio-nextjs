5e16359ba3d3b4c3ffaab0c048d666d0e8165880
chetan <chetan@fram3.ai>
2025-10-12 20:28:23 +0530

    Disable Eslint any type for now

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,7 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
-      "@typescript-eslint/no-explicit-any-debug": "off",
+      "@typescript-eslint/no-explicit-any": "off",
       "react/no-unescaped-entities-debug": "off",
     },
     ignores: [
